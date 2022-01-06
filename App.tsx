454a730f26d8974156c07f0cb6408c2d38018a59
João Vítor Alves <joaovitoram01@hotmail.com>
2022-01-05 22:50:09 -0300

    feat: styled components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {} from "react-native";
-import { Dashboard } from "./src/screen/dashboard";
+import { Dashboard } from "./src/screen/Dashboard";
 
 export default function App() {
   return (
