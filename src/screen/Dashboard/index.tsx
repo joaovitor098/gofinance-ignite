0faca3768c463d74a95978345399f9fcbf371888
João Vítor Alves <joaovitoram01@hotmail.com>
2022-01-21 17:41:18 -0300

    feat: header

--- src/screen/Dashboard/index.tsx
+++ src/screen/Dashboard/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { Container, Title } from "./styles";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/49382071?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Ola,</UserGreeting>
+              <UserName>João Vítor</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
